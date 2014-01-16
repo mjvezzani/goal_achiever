7f21f1f77f6162df4ec5f311c817a0b79f3afea4
mjvezzani <mjvezzani@gmail.com>
2014-01-16 06:53:26 -0500

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoalAchiever::Application.config.secret_key_base = 'c7dbb6ca0b67cc8fb75d8b17bdff26c5588f8de099f4b376cc5dab93692f2dd5cb01ffbe4795e8224dcfe945cafa896163760ead358a5e1b6a215dc947feb745'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoalAchiever::Application.config.session_store :cookie_store, key: '_goal_achiever_session'
